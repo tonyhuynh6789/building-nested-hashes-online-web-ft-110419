4f44cd700217c8340d0627e2da9db689384a9185
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-10 01:15:20 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,12 +5,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {:name => "Benvolio",
+        {
+        :name => "Benvolio",
         :age => "17",
-        :attitude => "worried"},
-        {:name => "Mercutio",
+        :attitude => "worried"
+        },
+        {
+        :name => "Mercutio",
         :age => "18",
-        :attitude => "hot-headed"}
+        :attitude => "hot-headed"
+        }
       ]
    }, 
    :capulet => {
@@ -18,7 +22,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        
       ]
    }
 }
